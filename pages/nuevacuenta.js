a75788b16a8e8b673ea1216b1247bdb97a6b75d0
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-12 13:27:17 -0600

    Add the view of products, orders, login and new account.

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const NuevaCuenta = () => {
+	return (
+		<React.Fragment>
+			<Layout>
+				<h1 className="text-center text-2xl text-white font-light">Nueva Cuenta</h1>
+			</Layout>
+		</React.Fragment>
+	);
+};
+
+export default NuevaCuenta;
