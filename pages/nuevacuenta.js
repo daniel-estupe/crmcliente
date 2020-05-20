00a71d8e02a7b418f6c1b10ec30657986a0bab23
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-20 11:15:45 -0600

    Configure to create new accounts.

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import { useMutation, gql } from '@apollo/client';
+
+const NUEVA_CUENTA = gql`
+	mutation nuevoUsuario($input: UsuarioInput) {
+		nuevoUsuario(input: $input) {
+			id
+			nombre
+			apellido
+			email
+		}
+	}
+`;
 
 const NuevaCuenta = () => {
+	const [ mensaje, guardarMensaje ] = useState(null);
+	const [ nuevoUsuario ] = useMutation(NUEVA_CUENTA);
+	const router = useRouter();
+
 	// validación del formulario
 	const formik = useFormik({
 		initialValues: {
@@ -20,14 +37,48 @@ const NuevaCuenta = () => {
 				.required('El password es requerido')
 				.min(6, 'El password debe ser de al menos 6 caracteres.')
 		}),
-		onSubmit: (valores) => {
-			console.log('values', valores);
+		onSubmit: async (valores) => {
+			const { nombre, apellido, email, password } = valores;
+			try {
+				const { data } = await nuevoUsuario({
+					variables: {
+						input: {
+							nombre,
+							apellido,
+							email,
+							password
+						}
+					}
+				});
+
+				guardarMensaje(`Se ha creado correctamente el usuario: ${data.nuevoUsuario.nombre}`);
+
+				setTimeout(() => {
+					guardarMensaje(null);
+					router.push('/login');
+				}, 3000);
+			} catch (error) {
+				guardarMensaje(error.message.replace('GraphQL error: ', ''));
+
+				setTimeout(() => {
+					guardarMensaje(null);
+				}, 3000);
+			}
 		}
 	});
+
+	const mostrarMensaje = () => {
+		return (
+			<div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+				<p>{mensaje}</p>
+			</div>
+		);
+	};
 
 	return (
 		<React.Fragment>
 			<Layout>
+				{mensaje && mostrarMensaje()}
 				<h1 className="text-center text-2xl text-white font-light">Crear Nueva Cuenta</h1>
 				<div className="flex justify-center mt-5">
 					<div className="w-full max-w-sm">
