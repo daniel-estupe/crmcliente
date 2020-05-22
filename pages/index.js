8ca5bd0079fa03c16515405fbe1a711e45ab647c
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-22 08:47:45 -0600

    Create the new client form and set the reference.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import { gql, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const OBTENER_CLIENTES = gql`
 	query obtenerClientesVendedor {
@@ -29,6 +30,11 @@ const Index = () => {
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
 
+			<Link href="/nuevocliente">
+				<a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">
+					Nuevo Cliente
+				</a>
+			</Link>
 			<table className="table-auto shadow-md mt-10 w-full w-lg">
 				<thead className="bg-gray-800">
 					<tr className="text-white">
