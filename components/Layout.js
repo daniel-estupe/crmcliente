f9a4c77a42706c52b7257e8d7fe07e01fc315282
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-21 22:31:21 -0600

    Add header to show the user name and logout button.

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
 import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
@@ -27,7 +28,10 @@ const Layout = ({ children }) => {
 				<div className="bg-gray-200 min-h-screen">
 					<div className="flex min-h-screen">
 						<Sidebar />
-						<main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">{children}</main>
+						<main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+							<Header />
+							{children}
+						</main>
 					</div>
 				</div>
 			)}
