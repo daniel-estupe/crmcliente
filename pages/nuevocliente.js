181a281c347c0e230eb64fcf30b838b1f03c9c74
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-22 09:22:25 -0600

    Add validations to the new client form.

--- pages/nuevocliente.js
+++ pages/nuevocliente.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const NuevoCliente = () => {
+	const formik = useFormik({
+		initialValues: {
+			nombre: '',
+			apellido: '',
+			empresa: '',
+			email: '',
+			telefono: ''
+		},
+		validationSchema: Yup.object({
+			nombre: Yup.string().required('El nombre del cliente es obligatorio'),
+			apellido: Yup.string().required('El apellido del cliente es obligatorio'),
+			empresa: Yup.string().required('El empresa del cliente es obligatorio'),
+			email: Yup.string().email('Email no es válido.').required('El email del cliente es obligatorio')
+		}),
+		onSubmit: (valores) => {
+			console.log(valores);
+		}
+	});
+
 	return (
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Nuevo Cliente</h1>
 
 			<div className="flex justify-center mt-5">
 				<div className="w-full max-w-lg">
-					<form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
+					<form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
 						<div className="mb-4">
 							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
 								Nombre
@@ -18,7 +39,16 @@ const NuevoCliente = () => {
 								id="nombre"
 								type="text"
 								autoComplete="off"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.nombre}
 							/>
+							{formik.touched.nombre && formik.errors.nombre ? (
+								<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+									<p className="font-bold">Error</p>
+									<p>{formik.errors.nombre}</p>
+								</div>
+							) : null}
 						</div>
 						<div className="mb-4">
 							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
@@ -29,7 +59,16 @@ const NuevoCliente = () => {
 								id="apellido"
 								type="text"
 								autoComplete="off"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.apellido}
 							/>
+							{formik.touched.apellido && formik.errors.apellido ? (
+								<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+									<p className="font-bold">Error</p>
+									<p>{formik.errors.apellido}</p>
+								</div>
+							) : null}
 						</div>
 						<div className="mb-4">
 							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="empresa">
@@ -40,7 +79,16 @@ const NuevoCliente = () => {
 								id="empresa"
 								type="text"
 								autoComplete="off"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.empresa}
 							/>
+							{formik.touched.empresa && formik.errors.empresa ? (
+								<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+									<p className="font-bold">Error</p>
+									<p>{formik.errors.empresa}</p>
+								</div>
+							) : null}
 						</div>
 						<div className="mb-4">
 							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -51,7 +99,16 @@ const NuevoCliente = () => {
 								id="email"
 								type="email"
 								autoComplete="off"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.email}
 							/>
+							{formik.touched.email && formik.errors.email ? (
+								<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+									<p className="font-bold">Error</p>
+									<p>{formik.errors.email}</p>
+								</div>
+							) : null}
 						</div>
 						<div className="mb-4">
 							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
@@ -62,6 +119,9 @@ const NuevoCliente = () => {
 								id="telefono"
 								type="tel"
 								autoComplete="off"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.telefono}
 							/>
 						</div>
 
