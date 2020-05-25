f97b714bfa7599008a89b6b0e1af04195d528740
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-25 12:30:20 -0600

    Add functionality to add new products.

--- pages/productos.js
+++ pages/productos.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import Producto from '../components/Producto';
 import { gql, useQuery } from '@apollo/client';
+import Link from 'next/link';
 
 const OBTENER_PRODUCTOS = gql`
 	query obtenerProductos {
@@ -21,6 +22,11 @@ export default function Productos() {
 	return (
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+			<Link href="/nuevoproducto">
+				<a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">
+					Nuevo Producto
+				</a>
+			</Link>
 			<table className="table-auto shadow-md mt-10 w-full w-lg">
 				<thead className="bg-gray-800">
 					<tr className="text-white">
