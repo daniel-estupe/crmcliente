a5e3aacaa292577895d394ba7fb1397482c32eb7
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-26 18:55:16 -0600

    Configure to get customers on the new order form.

--- components/pedidos/AsignarCliente.js
+++ components/pedidos/AsignarCliente.js
@@ -1,14 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Select from 'react-select';
+import { gql, useQuery } from '@apollo/client';
+import PedidoContext from '../../context/pedidos/PedidoContext';
 
-const clientes = [ { id: 1, nombre: 'Juan' }, { id: 2, nombre: 'Pablo' }, { id: 3, nombre: 'Daniel' } ];
+const OBTENER_CLIENTES = gql`
+	query obtenerClientesVendedor {
+		obtenerClientesVendedor {
+			id
+			nombre
+			apellido
+			empresa
+			email
+		}
+	}
+`;
 
 const AsignarCliente = () => {
 	const [ cliente, setClientes ] = useState([]);
 
+	const pedidoContext = useContext(PedidoContext);
+	const { agregarCliente } = pedidoContext;
+
+	const { data, loading, error } = useQuery(OBTENER_CLIENTES);
+
 	useEffect(
 		() => {
-			console.log(cliente);
+			agregarCliente(cliente);
 		},
 		[ cliente ]
 	);
@@ -17,16 +34,27 @@ const AsignarCliente = () => {
 		setClientes(clientes);
 	};
 
+	if (loading) {
+		return null;
+	}
+
+	const { obtenerClientesVendedor } = data;
+
 	return (
-		<Select
-			options={clientes}
-			isMulti={true}
-			onChange={(opcion) => seleccionarCliente(opcion)}
-			getOptionValue={(opcion) => opcion.id}
-			getOptionLabel={(opcion) => opcion.nombre}
-			placeholder="Busque o Seleccione el cliente"
-			noOptionsMessage={() => 'No hay resultados'}
-		/>
+		<React.Fragment>
+			<p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-2 text-sm font-bold">
+				1.- Asigna un Cliente a un pedido
+			</p>
+			<Select
+				className="mt-3"
+				options={obtenerClientesVendedor}
+				onChange={(opcion) => seleccionarCliente(opcion)}
+				getOptionValue={(opcion) => opcion.id}
+				getOptionLabel={(opcion) => opcion.nombre}
+				placeholder="Busque o Seleccione el cliente"
+				noOptionsMessage={() => 'No hay resultados'}
+			/>
+		</React.Fragment>
 	);
 };
 
