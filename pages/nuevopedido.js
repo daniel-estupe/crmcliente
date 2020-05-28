f7876220322b9d1f01f37c5b93c6fe5d535c1ddd
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-27 23:54:47 -0600

    Add resume component to the new order view.

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Layout from '../components/Layout';
 import AsignarCliente from '../components/pedidos/AsignarCliente';
 import AsignarProductos from '../components/pedidos/AsignarProductos';
+import ResumenPedido from '../components/pedidos/ResumenPedido';
 
 import PedidoContext from '../context/pedidos/PedidoContext';
 
@@ -11,8 +12,13 @@ const NuevoPedido = () => {
 	return (
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Nuevo Pedido</h1>
-			<AsignarCliente />
-			<AsignarProductos />
+			<div className="flex justify-center mt-5">
+				<div className="w-full max-w-lg">
+					<AsignarCliente />
+					<AsignarProductos />
+					<ResumenPedido />
+				</div>
+			</div>
 		</Layout>
 	);
 };
