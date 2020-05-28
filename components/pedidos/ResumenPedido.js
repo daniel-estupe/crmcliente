be40f3baf976c01b53724ee4d33826c098ca6c92
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-27 23:53:59 -0600

    Create a component to display the selected products.

---
+++ components/pedidos/ResumenPedido.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ResumenPedido = () => {
+	return (
+		<React.Fragment>
+			<p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-2 text-sm font-bold">
+				3.- Ajusta las cantidades del producto
+			</p>
+		</React.Fragment>
+	);
+};
+
+export default ResumenPedido;
