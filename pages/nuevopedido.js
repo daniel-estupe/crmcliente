caae75e4661a5518aa4d8a4de6de652cedfbdf53
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-05-31 21:27:54 -0600

    Add the total order component and the register button.

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import AsignarCliente from '../components/pedidos/AsignarCliente';
 import AsignarProductos from '../components/pedidos/AsignarProductos';
 import ResumenPedido from '../components/pedidos/ResumenPedido';
+import Total from '../components/pedidos/Total';
 
 import PedidoContext from '../context/pedidos/PedidoContext';
 
@@ -17,6 +18,13 @@ const NuevoPedido = () => {
 					<AsignarCliente />
 					<AsignarProductos />
 					<ResumenPedido />
+					<Total />
+					<button
+						type="button"
+						className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900`}
+					>
+						Registrar Pedido
+					</button>
 				</div>
 			</div>
 		</Layout>
