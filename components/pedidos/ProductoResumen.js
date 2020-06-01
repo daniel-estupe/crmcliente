0c6bb8d94e0cea944d90bee55a72a83223850569
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-06-01 11:27:25 -0600

    Add a handler when the quantity of the selected products changes.

--- components/pedidos/ProductoResumen.js
+++ components/pedidos/ProductoResumen.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import PedidoContext from '../../context/pedidos/PedidoContext';
 
 const ProductoResumen = ({ producto }) => {
+	const pedidoContext = useContext(PedidoContext);
+	const { cantidadProductos } = pedidoContext;
+
+	const [ cantidad, setCantidad ] = useState(0);
+
+	useEffect(
+		() => {
+			actualizarCantidad();
+		},
+		[ cantidad ]
+	);
+
+	const actualizarCantidad = () => {
+		const nuevoProducto = { ...producto, cantidad: Number(cantidad) };
+		cantidadProductos(nuevoProducto);
+	};
+
 	const { nombre, precio } = producto;
 
 	return (
@@ -13,6 +31,8 @@ const ProductoResumen = ({ producto }) => {
 				type="number"
 				placeholder="Cantidad"
 				className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline md:ml-4"
+				onChange={(e) => setCantidad(e.target.value)}
+				value={cantidad}
 			/>
 		</div>
 	);
