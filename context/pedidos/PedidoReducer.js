4807026ba999476d0b279277a222457bf2834949
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-06-01 18:00:29 -0600

    Add an action when the case is ACTUALIZAR_TOTAL.

--- context/pedidos/PedidoReducer.js
+++ context/pedidos/PedidoReducer.js
@@ -1,4 +1,4 @@
-import { SELECCIONAR_CLIENTE, SELECCIONAR_PRODUCTO, CANTIDAD_PRODUCTOS } from '../../types';
+import { SELECCIONAR_CLIENTE, SELECCIONAR_PRODUCTO, CANTIDAD_PRODUCTOS, ACTUALIZAR_TOTAL } from '../../types';
 
 export default (state, action) => {
 	switch (action.type) {
@@ -17,6 +17,14 @@ export default (state, action) => {
 				...state,
 				productos: state.productos.map(
 					(producto) => (producto.id === action.payload.id ? (producto = action.payload) : producto)
+				)
+			};
+		case ACTUALIZAR_TOTAL:
+			return {
+				...state,
+				total: state.productos.reduce(
+					(nuevoTotal, articulo) => (nuevoTotal += articulo.precio * articulo.cantidad),
+					0
 				)
 			};
 		default:
