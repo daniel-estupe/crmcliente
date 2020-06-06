ac0b355f402a5c5031214df8dbb4b27e52763422
Daniel Estupe <estupe.gonzalez@gmail.com>
2020-06-05 21:09:05 -0600

    Display charts of top sellers.

--- pages/mejoresvendedores.js
+++ pages/mejoresvendedores.js
@@ -15,66 +15,30 @@ const MEJORES_VENDEDORES = gql`
 	}
 `;
 
-const datos = [
-	{
-		name: 'Page A',
-		uv: 4000,
-		pv: 2400,
-		amt: 2400
-	},
-	{
-		name: 'Page B',
-		uv: 3000,
-		pv: 1398,
-		amt: 2210
-	},
-	{
-		name: 'Page C',
-		uv: 2000,
-		pv: 9800,
-		amt: 2290
-	},
-	{
-		name: 'Page D',
-		uv: 2780,
-		pv: 3908,
-		amt: 2000
-	},
-	{
-		name: 'Page E',
-		uv: 1890,
-		pv: 4800,
-		amt: 2181
-	},
-	{
-		name: 'Page F',
-		uv: 2390,
-		pv: 3800,
-		amt: 2500
-	},
-	{
-		name: 'Page G',
-		uv: 3490,
-		pv: 4300,
-		amt: 2100
-	}
-];
-
 const MejoresVendedores = () => {
 	const { data, loading, error } = useQuery(MEJORES_VENDEDORES);
 
 	if (loading) return 'Cargando...';
 
-	console.log('data', data);
+	const { mejoresVendedores } = data;
+
+	const vendedorGrafica = [];
+
+	mejoresVendedores.map((vendedor, index) => {
+		vendedorGrafica[index] = {
+			...vendedor.vendedor[0],
+			total: vendedor.total
+		};
+	});
 
 	return (
 		<Layout>
 			<h1 className="text-2xl text-gray-800 font-light">Mejores Vendedores</h1>
-			{/* <BarChart
+			<BarChart
 				className="mt-10"
 				width={600}
 				height={500}
-				data={datos}
+				data={vendedorGrafica}
 				margin={{
 					top: 5,
 					right: 30,
@@ -83,12 +47,12 @@ const MejoresVendedores = () => {
 				}}
 			>
 				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="name" />
+				<XAxis dataKey="nombre" />
 				<YAxis />
 				<Tooltip />
 				<Legend />
-				<Bar dataKey="pv" fill="#3182CE" />
-			</BarChart> */}
+				<Bar dataKey="total" fill="#3182CE" />
+			</BarChart>
 		</Layout>
 	);
 };
